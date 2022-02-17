867b716b5525ecf958c54728f36dd22dd1146d54
czdvoika <59421225+czdvoika@users.noreply.github.com>
2022-02-17 20:56:44 +0100

    Update text_extractor.py

--- text_extractor.py
+++ text_extractor.py
@@ -1,5 +1,5 @@
 s = open("save.txt", "w")
-f = open("mourek.core","rb",)
+f = open("menu.core","rb",)
 i = 1
 off = 0
 textak = f.read()
@@ -7,29 +7,15 @@ textak = f.read()
 while i < 100: 
   
   pozice1 = textak.find(b"\xE2\xB2\x0B\x42\x6B\x59\x9A\xB8", off)
-  print(pozice1)
   offset = f.seek(pozice1+28) 
-  print(f.tell())
-  
   size = f.read(1)
   nic = f.read(1) 
-  print(size)
-  usize = ord(size)
-  print(usize)
- 
+  usize = ord(size) 
   text = (f.read(usize))
   finish = text.decode('utf-8')
   enko = finish.replace("\n","\\n")
-  print(enko)
   s.write(enko)
+  print(enko)
   s.write("\n")
-  
   off = offset
-  print(off)
   i += 1
- 
-
- 
- 
- 
- 
