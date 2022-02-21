4328c25a71a8d0f990fdc5babd258b1c8f1e116d
czdvoika <59421225+czdvoika@users.noreply.github.com>
2022-02-21 22:36:02 +0100

    Update importbin.py

--- importbin.py
+++ importbin.py
@@ -1,5 +1,12 @@
 fh = open("more.core", "r+b")
-fh.seek(4060)
+bin = fh.read()
+pozice1 = bin.find(b"\xE2\xB2\x0B\x42\x6B\x59\x9A\xB8")
+print(pozice1)
+text = fh.seek(pozice1+28)
+print(text)
+size = fh.read(1)
+nic = fh.read(1)
+fh.write(b"picak")
 
-fh.write(b"kokot")
+
 fh.close()
