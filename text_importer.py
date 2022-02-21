ab192897a2b7d5bc33e74344fddccff6f7dac98c
czdvoika <59421225+czdvoika@users.noreply.github.com>
2022-02-21 23:16:05 +0100

    Update text_importer.py

--- text_importer.py
+++ text_importer.py
@@ -1,15 +1,20 @@
 core = open("more.core", "r+b")
 txt =  open("save.txt", "rb")
 
+
 bin = core.read()
 pozice1 = bin.find(b"\xE2\xB2\x0B\x42\x6B\x59\x9A\xB8")
 print(pozice1)
 text = core.seek(pozice1+28)
 print(text)
 size = core.read(1)
+usize = ord(size)
+print(usize)
 nic = core.read(1)
 radek = txt.readline()
+print(radek)
 core.write(radek)
 
-txt.close()
+
 core.close()
+txt.close()
