05dbd943806a60879b72d36258406b88a3fe47ae
czdvoika <59421225+czdvoika@users.noreply.github.com>
2022-02-24 00:33:44 +0100

    Update importtest.py

--- importtest.py
+++ importtest.py
@@ -1,25 +1,47 @@
-core = open("simpletext_pc.core", "rb")
-corewrite = open("simpletext_pc.core", "wb")
-txt =  open("save.txt", "r")
-binar = core.read()
-line = txt.readline()
+with open('save.txt', encoding='utf8') as text:
+ line = text.readline()
+ print(line)
+ size = len(line)
+ nsize = (size-1)
+ print(nsize)
+ hexsize = hex(nsize)
+ print(hexsize)
+ hotovo = hexsize.replace("0x","\\x")
+ print(hotovo)
+ 
 
-binline = line.encode("utf-8")
-print(binline)
-size = len(binline)
-nsize = (size-1)
-print(nsize)
+with open('save.core', 'w') as save:
+ 
+ save.write(hotovo)
 
-usize = chr(nsize)
-print(usize)
-hexsize = hex(nsize)
-print(hexsize)
+# [2:]
+# core = open("simpletext_pc.core", "rb")
+# txt =  open("save.txt", "r", "utf-8")
 
-finish = usize.encode("utf-8")
-print(finish)
+# line = txt.readline()
+# print(line)
+
+# print(binline)
+# size = len(binline)
+# nsize = (size-1)
+# print(nsize)
+
+# char = chr(nsize)
+# print(char)
+
+
+# usize = chr(nsize)
+# enusize = usize.encode("utf-8")
+# print(enusize)
+# hexsize = hex(nsize)
+# print(hexsize)
+
+# finish = usize.encode("utf-8")
+# print(finish)
 
 # pozice1 = binar.find(b"\xE2\xB2\x0B\x42\x6B\x59\x9A\xB8")
 # print(pozice1)
 # offset = core.seek(pozice1+28)
 # print(offset)
+
 
