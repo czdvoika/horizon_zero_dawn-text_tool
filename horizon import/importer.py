7866475178ae4bddff464cccc34b773ffc3ad015
czdvoika <59421225+czdvoika@users.noreply.github.com>
2022-02-25 12:30:51 +0100

    Update importer.py

--- horizon import/importer.py
+++ horizon import/importer.py
@@ -1,19 +1,22 @@
 s = open("save.txt", "r")
 f = open("save.core", "r+b")
 
+
 line = s.readline()
 print(line)
+print("start",f.tell())
 size = len(line)
-print(size)
+print("size",size)
 nsize = (size-1)
-print(nsize)
+print("nsize",nsize)
 usize = hex(nsize)
-print(usize)
+print("hexsize",usize)
 enusize = nsize.to_bytes(2, 'little')
-print(enusize)
+print("hexsize",enusize)
 f.write(enusize)
 encodeline = line.encode("utf-8")
 f.write(encodeline)
+print("end",f.tell())
 
 
 
