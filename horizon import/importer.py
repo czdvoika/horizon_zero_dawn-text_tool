3e293e058ae11a3a760ef912cd1f336e3f4a83e2
czdvoika <59421225+czdvoika@users.noreply.github.com>
2022-02-26 01:08:53 +0100

    Update importer.py

--- horizon import/importer.py
+++ horizon import/importer.py
@@ -1,18 +1,42 @@
-s = open("save.txt", "r")
-f = open("save.core", "r+b")
+s = open("save_czech.txt", "r", encoding = "utf-8" )
+f = open("simpletext_pc.core", "r+b")
+i = 1
+off = 0
+textak = f.read()
 
-line = s.readline()
-print(line)
-print("start",f.tell())
-size = len(line)
-print("size",size)
-nsize = (size-1)
-print("nsize",nsize)
-usize = hex(nsize)
-print("hexsize",usize)
-enusize = nsize.to_bytes(2, 'little')
-print("hexsize",enusize)
-f.write(enusize)
-encodeline = line.encode("utf-8")
-f.write(encodeline)
-print("end",f.tell())
+while i < 823: 
+ pozice1 = textak.find(b"\xE2\xB2\x0B\x42\x6B\x59\x9A\xB8", off)
+ offset = f.seek(pozice1+28)
+
+ line = s.readline()
+ print(line)
+ start = f.tell()
+ print("start",f.tell())
+
+ encodeline = line.encode("utf-8")
+ f.write(b"..")
+ f.write(encodeline)
+ end = f.tell()
+ print("end",f.tell())
+ f.write(b'\x02')
+
+ sizeoff = (end - start - 2)
+ print(sizeoff)
+
+ usize = hex(sizeoff)
+ print("hexsize",usize)
+
+ enusize = sizeoff.to_bytes(2, 'little')
+ print("hexsize",enusize)
+ f.seek(start)
+ f.write(enusize)
+ 
+ print(start)
+
+ off = offset
+ i += 1
+
+s.close()
+f.close() 
+
+
