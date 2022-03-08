98d6922a78b108e5930b29f0092b4c67181cca72
czdvoika <59421225+czdvoika@users.noreply.github.com>
2022-03-08 22:59:46 +0100

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,3 +1,15 @@
+import sys
+s = open(sys.argv[1], "r", encoding = "utf-8" )
+f = open(sys.argv[2], "w")
+textak = s.read()
+
 from deep_translator import GoogleTranslator
-translated = GoogleTranslator(source='auto', target='czech').translate_file('save.txt')
-print(translated)
+translator = GoogleTranslator(source='auto',target='czech')
+
+text_to_translate = textak
+
+translated_text = translator.translate(text_to_translate)
+
+print(translated_text)
+
+f.write(translated_text)
