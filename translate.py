1efe3fc378aebef1a02e82cd5b64bb1132500ae8
czdvoika <59421225+czdvoika@users.noreply.github.com>
2022-06-01 21:07:20 +0200

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,15 +1,21 @@
 import sys
 s = open(sys.argv[1], "r", encoding = "utf-8" )
 f = open(sys.argv[2], "w")
-textak = s.read()
+i = 1
 
-from deep_translator import GoogleTranslator
-translator = GoogleTranslator(source='auto',target='czech')
 
-text_to_translate = textak
+while i <22:
+ textak = s.readline()
 
-translated_text = translator.translate(text_to_translate)
+ from deep_translator import GoogleTranslator
+ translator = GoogleTranslator(source='slovak',target='czech')
 
-print(translated_text)
+ text_to_translate = textak
 
-f.write(translated_text)
+ translated_text = translator.translate(text_to_translate)
+
+ print(translated_text)
+
+ f.write(translated_text)
+ f.write("\n")
+ i += 1
